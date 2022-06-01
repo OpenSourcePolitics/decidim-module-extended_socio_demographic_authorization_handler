058db10e60ed60a622f7a2e58f4c26dd822f8487
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-06-01 13:25:17 +0200

    Add 'code-postaux' lib and implement in select field

--- app/packs/src/decidim/address.js
+++ app/packs/src/decidim/address.js
@@ -1,8 +1,14 @@
+const codesPostaux = require('codes-postaux');
+
 $("#authorization_handler_postal_code").on("input", (e) => {
-    var $element = $(e.currentTarget);
-    var value = $element.val();
+    const $element = $(e.currentTarget);
+    const value = $element.val();
 
     if (value.length > 4) {
-        console.log(value);
+        var cities = codesPostaux.find(value);
+
+        for (var i = 0; i < cities.length; i++) {
+            $("#authorization_handler_city").append(`<option value="${cities[i].libelleAcheminement}">${cities[i].libelleAcheminement}</option>`)
+        }
     }
 })
