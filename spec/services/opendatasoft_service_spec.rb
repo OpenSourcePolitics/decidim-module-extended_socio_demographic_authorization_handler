76c21e673523a6c3173ac335ab06a23365894f9b
quentinchampenois <quentin.champenois7@gmail.com>
2023-06-22 15:06:27 +0200

    Fix rubocop offenses

--- spec/services/opendatasoft_service_spec.rb
+++ spec/services/opendatasoft_service_spec.rb
@@ -1,30 +1,32 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require "spec_helper"
 
 describe OpendatasoftService do
-  let(:zipcode) { '12345' }
+  let(:zipcode) { "12345" }
   let(:organization) { create :organization }
   let(:service) { OpendatasoftService.new(zipcode, organization) }
 
-  describe '#fetch_cities!' do
-    context 'when the zipcode is blank' do
-      let(:zipcode) { '' }
+  describe "#fetch_cities!" do
+    context "when the zipcode is blank" do
+      let(:zipcode) { "" }
 
-      it 'returns an empty array' do
+      it "returns an empty array" do
         expect(service.fetch_cities!).to eq([])
       end
     end
 
-    context 'when the zipcode is invalid' do
-      let(:zipcode) { '123' }
+    context "when the zipcode is invalid" do
+      let(:zipcode) { "123" }
 
-      it 'returns an empty array' do
+      it "returns an empty array" do
         expect(service.fetch_cities!).to eq([])
       end
     end
 
-    context 'when the zipcode is valid' do
-      let(:zipcode) { '12345' }
-      let(:cache_key) { 'opendatasoft/12345' }
+    context "when the zipcode is valid" do
+      let(:zipcode) { "12345" }
+      let(:cache_key) { "opendatasoft/12345" }
       let(:response_body) do
         {
           "records" => [
@@ -33,18 +35,18 @@ describe OpendatasoftService do
           ]
         }
       end
-      let(:response) { double('Response', to_hash: { body: response_body.to_json }) }
+      let(:response) { double("Response", to_hash: { body: response_body.to_json }) }
 
       before do
         allow(Rails.cache).to receive(:fetch).and_yield
         allow(Faraday).to receive(:get).and_return(response)
       end
 
-      it 'returns the cities extracted from the API response' do
-        expect(service.fetch_cities!).to eq(['City 1', 'City 2'])
+      it "returns the cities extracted from the API response" do
+        expect(service.fetch_cities!).to eq(["City 1", "City 2"])
       end
 
-      it 'caches the response' do
+      it "caches the response" do
         expect(Rails.cache).to receive(:fetch).with(cache_key, expires_in: 15.days).and_yield
 
         service.fetch_cities!
